b591a03d4d9579ddad0c80417e3f1104be4ac135
Roland Praml <roland.praml@foconis.de>
2023-09-13 16:24:38 +0200

    Updated to java 11 with module info and merged fixes (#4)
    * Set cannot handle wrapped java objects properly
    
    * Satisfy spotbugs
    
    * bump java source and target version to 11
    
    * Upgraded gradle to 8.3
    
    * reverted gradle
    
    * Fix Module-Info
    
    * removed java 8 from CI
    
    * excluded module-info from checkstyle
    
    * FIX: Spotbugs
    
    * Fix for Set.equals
    
    * removed java 8 workflow

---
+++ src/module-info.java
@@ -0,0 +1,38 @@
+module org.mozilla.rhino {
+    exports org.mozilla.javascript;
+    exports org.mozilla.javascript.annotations;
+    // exports org.mozilla.javascript.ast;
+
+    exports org.mozilla.javascript.commonjs.module;
+    exports org.mozilla.javascript.commonjs.module.provider;
+    exports org.mozilla.javascript.debug;
+
+    // optimizer has to be exported for compiled classes
+    exports org.mozilla.javascript.optimizer;
+
+    // engine not part of the runtime
+    // exports org.mozilla.javascript.engine;
+
+    // exports org.mozilla.javascript.json;
+    // exports org.mozilla.javascript.optimizer;
+    // exports org.mozilla.javascript.regexp;
+    exports org.mozilla.javascript.serialize;
+    exports org.mozilla.javascript.typedarrays;
+    exports org.mozilla.javascript.xml;
+
+    // from the toolsrc dir (not part of the runtime)
+    // exports org.mozilla.javascript.tools;
+    // exports org.mozilla.javascript.tools.debugger;
+    // exports org.mozilla.javascript.tools.jsc;
+    // exports org.mozilla.javascript.tools.shell;
+
+    // required for (optional) property-change support
+    requires static java.desktop;
+
+    // required for (optional) ScriptEngineFactory
+    requires static java.scripting;
+
+// not part of the runtime
+// provides javax.script.ScriptEngineFactory with
+//        org.mozilla.javascript.engine.RhinoScriptEngineFactory;
+}
