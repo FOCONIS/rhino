a1271c06875a35c8daebef08c023d8013d19a45f
Roland Praml <roland.praml@foconis.de>
2023-09-13 09:35:16 +0200

    reverted gradle

--- src/module-info.java
+++ src/module-info.java
@@ -1,22 +1,32 @@
 module org.mozilla.rhino {
-	exports org.mozilla.javascript;
-	exports org.mozilla.javascript.annotations;
-	exports org.mozilla.javascript.tools.shell;
-	exports org.mozilla.javascript.ast;
-	exports org.mozilla.javascript.tools;
-	exports org.mozilla.javascript.commonjs.module;
-	exports org.mozilla.javascript.commonjs.module.provider;
-	exports org.mozilla.javascript.typedarrays;
-	exports org.mozilla.javascript.optimizer;
-	exports org.mozilla.javascript.engine;
-	exports org.mozilla.javascript.serialize;
-	exports org.mozilla.javascript.json;
-	exports org.mozilla.classfile;
-	// required for (optional) property-change support
-	requires static java.desktop;
+    exports org.mozilla.javascript;
+    exports org.mozilla.javascript.annotations;
+    // exports org.mozilla.javascript.ast;
 
-	// required for (optional) ScriptEngineFactory
-	requires static java.scripting;
+    exports org.mozilla.javascript.commonjs.module;
+    exports org.mozilla.javascript.commonjs.module.provider;
+    exports org.mozilla.javascript.debug;
+    exports org.mozilla.javascript.engine;
 
-	provides javax.script.ScriptEngineFactory with org.mozilla.javascript.engine.RhinoScriptEngineFactory;
+    // exports org.mozilla.javascript.json;
+    // exports org.mozilla.javascript.optimizer;
+    // exports org.mozilla.javascript.regexp;
+    exports org.mozilla.javascript.serialize;
+    exports org.mozilla.javascript.typedarrays;
+    exports org.mozilla.javascript.xml;
+
+    // from the toolsrc dir
+    exports org.mozilla.javascript.tools;
+    exports org.mozilla.javascript.tools.debugger;
+    exports org.mozilla.javascript.tools.jsc;
+    exports org.mozilla.javascript.tools.shell;
+
+    // required for (optional) property-change support
+    requires static java.desktop;
+
+    // required for (optional) ScriptEngineFactory
+    requires static java.scripting;
+
+    provides javax.script.ScriptEngineFactory with
+            org.mozilla.javascript.engine.RhinoScriptEngineFactory;
 }
